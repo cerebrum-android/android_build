5401bec939350204539c6413a3675621b150fd44
Mihai Costea <mihai.andrei.costea@gmail.com>
2012-09-20 12:29:18 +0300

    Brains, baby...

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,8 +185,8 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
   $(call import-products,$(call get-product-makefiles,\
       $(SRC_TARGET_DIR)/product/AndroidProducts.mk))
 else
-  ifneq ($(AOSP_BUILD),)
-    $(call import-products, device/*/$(AOSP_BUILD)/aosp.mk)
+  ifneq ($(CEREBRUM_BUILD),)
+    $(call import-products, device/*/$(CEREBRUM_BUILD)/cerebrum.mk)
   else
   # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
